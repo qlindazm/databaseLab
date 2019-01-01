a6b3619957d3cf081c5a73012da623444b10a828
qlindazm <qlindazm@bjfu.edu.cn>
2019-01-01 18:44:58 +0800

    add method int login(String,String)

--- DbUtil.java
+++ DbUtil.java
@@ -1,7 +1,7 @@
-
 import java.sql.*;
 import java.io.*;
-
+import java.text.*;
+import java.util.Date;
 import java.util.Properties;
 
 public class DbUtil{
@@ -10,7 +10,9 @@ public class DbUtil{
     private static String url;
     private static String user;
     private static String password;
- 
+
+    public String userID = null;
+    
 
     static {
         try {
@@ -35,8 +37,48 @@ public class DbUtil{
             e.printStackTrace();
         }
     }
-    public void test(){
-    	System.out.print(conn);
+    public static void release(){
+        if(conn!=null){
+            try{
+                conn.close();
+            }catch(SQLException e){
+                e.printStackTrace();
+            }
+        }
     }
-
+    /* check user when login
+     * no such user return -1
+     * password uncorrect return 0
+     * success return 1
+     */
+    public int login(String un, String pwd){
+    	Statement st = null;
+        ResultSet rs = null;
+        PreparedStatement ps = null;
+        int flag = -1;
+        try{
+            st = conn.createStatement();
+            rs = st.executeQuery("select * from user;");
+            while(rs.next() && flag==-1){
+                if(rs.getString("userID").equals(un)){
+                    flag = 0;
+                    if(rs.getString("password").equals(pwd))
+                        flag = 1;
+                }
+            }
+            if(flag==1){
+                ps = conn.prepareStatement("select readerid from userdetail where userid=?;");
+                ps.setString(1,un);
+                rs = ps.executeQuery();
+                if(rs.next())userID = rs.getString("readerid");
+            }
+            rs.close();
+            ps.close();
+            st.close();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return flag;
+    }
+    
 }
