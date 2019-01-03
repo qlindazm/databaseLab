6bb4b108807e9cb7cefe35dcdda3ca6a45ea38b5
Whteway <whteway1998@163.com>
2019-01-03 17:01:06 +0800

    add test code

---
+++ Test.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Test {
+	public static void main(String []args) {
+		DbUtil dbUtil = new DbUtil();
+		boolean isLoop = true;
+		while(isLoop) {
+			isLoop = menu(dbUtil);
+		}
+	}
+	
+	static boolean menu(DbUtil dbUtil){
+		System.out.println("-----图书管理系统-----");
+		System.out.println("1.登录");
+		System.out.println("2.借书");
+		System.out.println("3.还书");
+		System.out.println("4.查书");
+		System.out.println("5.显示历史借阅");
+		System.out.println("6.显示当前已借");
+		System.out.println("0.退出");
+		System.out.print("请输入标号：");
+		
+		Scanner scan = new Scanner(System.in);
+		char ch = scan.next().charAt(0);
+		System.out.println("");
+		
+		switch(ch) {
+			case '1':
+				System.out.print("用户名：");
+				String username = scan.next();
+				System.out.print("密码：");
+				String password = scan.next();
+				dbUtil.login(username,password);
+				break;
+			case '2':
+				System.out.print("书籍编号：");
+				String borrowcode = scan.nextLine();
+				dbUtil.borrowBook(borrowcode);
+				break;
+			case '3':
+				System.out.print("书籍编号：");
+				String returncode = scan.nextLine();
+				dbUtil.borrowBook(returncode);
+				break;
+			case '4':
+				System.out.print("书籍名称：");
+				String bookname = scan.nextLine();
+				String bookcode = dbUtil.searchByName(bookname);
+				if(bookcode != null)
+					System.out.println("该书可借，编号为："+bookcode);
+				else
+					System.out.println("该书不可借");
+				break;
+			case '5':
+				showBooks(dbUtil.showHistoryBorrowedBooks());
+				break;
+			case '6':
+				showBooks(dbUtil.showCurrentBorrowedBooks());
+				break;
+			case '0':
+				scan.close();
+				return false;
+			default:
+				System.out.println("请输入合法的字符");
+				break;
+		}
+		scan.close();
+		return true;
+	}
+	
+	//按表格形式显示书籍列表
+	static void showBooks(ArrayList<Book> books) {
+		System.out.println("编号\t书名\t作者\t出版社\t借出\t位置");
+		for(Book book:books)
+			System.out.println(book.barcode +"\t"+ book.bookName +"\t"+ book.author +"\t"+ book.publisher +"\t"+ book.isbn +"\t"+ book.position);
+	}
+	
+}
